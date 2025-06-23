72f75bdbd10d88e7428fa595984510818257e2dc
Naman Sharma <namansharam3194@gmail.com>
2025-06-24 00:14:49 +0530

    Refactor: Move backend files into backend folder

--- Backend/App.js
+++ Backend/App.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', require('./routes/AuthRoutes'));
 
 app.listen(5000, () => {
     console.log('Server started at port',5000);
