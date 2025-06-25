a8d0da3b5d6631a70d62552b46b28dfd2f38dfa3
Naman Sharma <namansharam3194@gmail.com>
2025-06-25 20:57:06 +0530

    Most of done

--- Backend/models/UserSchema.js
+++ Backend/models/UserSchema.js
@@ -5,14 +5,9 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     trim: true 
-<<<<<<< HEAD
   },
   email: {
     type: String,
-=======
-},
-  email:{type: String,
->>>>>>> 7860b08a7565a39ba9b2c6a3d232e38e48a46423
     required: true,
     unique: true
   },
@@ -25,7 +20,6 @@ const userSchema = new mongoose.Schema({
   role: {
     type: String,
     enum: ['user', 'admin'],
-<<<<<<< HEAD
     default: 'user'
   },
 
@@ -42,17 +36,5 @@ const userSchema = new mongoose.Schema({
   }
 
 }, { timestamps: true });
-=======
-  },
-
-  likedPosts: [
-    { type: mongoose.Schema.Types.ObjectId, ref: 'BlogPost' }
-  ],
-
-  bookmark: [
-    { type: mongoose.Schema.Types.ObjectId, ref: 'BlogPost' }
-  ]
-}, { timestamps: true });// This Timestamp add created at and updated at automaatically managed by Moongoose
->>>>>>> 7860b08a7565a39ba9b2c6a3d232e38e48a46423
 
 module.exports = mongoose.model('User', userSchema);
